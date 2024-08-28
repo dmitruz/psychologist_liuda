ebe714d074b77f67034415ffd0859de89caa36d6
dmitruz <dmitruz970@gmail.com>
2024-08-28 17:07:28 +0300

    kids section done

--- src/components/MyServices/MyServices.tsx
+++ src/components/MyServices/MyServices.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './MyServices.scss';
 import '../../styles/CallMeButton.scss';
 import selfEsteam from '../images/self-esteam.jpg';
+import familyKid from '../images/family.png';
 
 export const MyServices = () => {
     return (
@@ -24,28 +25,63 @@ export const MyServices = () => {
                 <p className="my-services__subtitle">Запити:</p>
                 <ul className="request-service__text">
                     <li className="request-service__item">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Qui, laborum quia cum fuga laboriosam cupiditate ipsam aut.
+                        Адаптація у новій країні
                     </li>
                     <li className="request-service__item">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Qui, laborum quia cum fuga laboriosam cupiditate ipsam aut.
+                        Пошук себе та професiй
                     </li>
                     <li className="request-service__item">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Qui, laborum quia cum fuga laboriosam cupiditate ipsam aut.
+                        Тайм-менеджемент та коучинг
+                    </li>
+                    <li className="request-service__item">
+                        Розлучення (супровід сім'ї)
+                    </li>
+                    <li className="request-service__item">
+                        Самооцінка та впевненість у собі
                     </li>
                 </ul>
+                <div className="call-me">
+                    <button className="call-me__btn">
+                        <span className="call-me__text">
+                            Зв'язок зі мною
+                        </span>
+                    </button>
+                </div>
             </div>
 
             <img src={selfEsteam} className="self-img" alt="self" />
-            <div className="call-me">
-                <button className="call-me__btn">
-                    <span className="call-me__text">
-                        Зв'язок зі мною
-                    </span>
-                </button>
-            </div>
+
+            <section className="kids-service">
+                <p className="kids-service__subtitle">Дітям та підліткам:</p>
+                <div className="kids-service__container">
+                    <ul className="kids-service__text">
+                        <li className="kids-service__item">
+                            Вікові особливості: криза школяра молодше 6 років, підліткова криза
+                        </li>
+                        <li className="kids-service__item">
+                            Погіршення успішності у школі
+                        </li>
+                        <li className="kids-service__item">
+                            Втрата інтересу до спілкування
+                        </li>
+                        <li className="kids-service__item">
+                            Замкнутість та підозрілість, втрата контакту з дорослими
+                        </li>
+                        <li className="kids-service__item">
+                            Залежність від гаджетів та соціальних мереж
+                        </li>
+                    </ul>
+                </div>
+                <img src={familyKid} className="family-img" alt="family" />
+                <div className="call-me">
+                    <button className="call-me__btn">
+                        <span className="call-me__text">
+                            Зв'язок зі мною
+                        </span>
+                    </button>
+                </div>
+            </section>
+
         </section>
     )
 }
